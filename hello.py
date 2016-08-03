8e4724b8c27ae3caf66e30c323ff8e828eb6b521
DevParadise <mobiledev72@gmail.com>
2016-08-03 20:36:24 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
+// For command line git tutorial
+// Github modification
 print("Hello, world!!!")
 print("Tell your world")
-<<<<<<< HEAD
 print("Tell his world")
-=======
 print("Tell her world")
->>>>>>> hotfix
